c8f38c04a2d839e2e840328e85b7c9856786f90a
Matheus Chagas <matheuschagas29@gmail.com>
2023-11-26 08:46:55 -0300

    add tutotial

--- js/login.js
+++ js/login.js
@@ -1,40 +1,63 @@
 const input = document.querySelector('.login__input');
-const button = document.querySelector('.login__button');
+const playButton = document.getElementById('playButton');
+const tutorialButton = document.getElementById('tutorialButton');
 const form = document.querySelector('.login-form');
+const tutorialPopup = document.getElementById('tutorialPopup');
+
+// Adicione uma variável de controle para verificar se o vídeo está sendo exibido
+let isVideoPlaying = false;
 
 const validateInput = ({ target }) => {
   if (target.value.length > 3) {
-    button.removeAttribute('disabled');
+    playButton.removeAttribute('disabled');
     return;
   }
 
-  button.setAttribute('disabled', '');
+  playButton.setAttribute('disabled', '');
 }
 
-const handleSubmit = (event) => {
+const handlePlay = (event) => {
   event.preventDefault();
 
   localStorage.setItem('player', input.value);
-  window.location = 'pages/game.html';
+  window.location.href = 'pages/game.html';
+}
+
+const showTutorial = () => {
+  // Seleciona o contêiner com a classe 'tutorial-popup-video'
+  const tutorialPopup = document.querySelector('.tutorial-popup-video');
+
+  // Exibe o contêiner alterando seu estilo para 'display: block'
+  tutorialPopup.style.display = 'block';
+
+  // Seleciona o elemento de vídeo dentro do contêiner
+  const videoElement = tutorialPopup.querySelector('video');
+
+  // Adiciona a propriedade 'autoplay' para iniciar o vídeo automaticamente
+  videoElement.setAttribute('autoplay', 'true');
+
+  // Atualiza a variável de controle
+  isVideoPlaying = true;
+
+  // Faça algo com o valor de videoSrc, se necessário
+  console.log('O vídeo está iniciando automaticamente.');
+}
+
+const closeTutorial = () => {
+  // Verifica se o vídeo está sendo exibido antes de fechar
+  if (isVideoPlaying) {
+    // Se o vídeo está sendo exibido, fecha o vídeo
+    tutorialPopup.style.display = 'none';
+    // Reseta a variável de controle
+    isVideoPlaying = false;
+  } else {
+    // Se o vídeo não está sendo exibido, redireciona para a página de login
+    window.location.href = 'login.html';
+  }
 }
 
 input.addEventListener('input', validateInput);
-form.addEventListener('submit', handleSubmit);
-
-function mostrarTutorial() {
-  var modal = document.getElementById("tutorialModal");
-  modal.style.display = "block";
-}
-
-function fecharTutorialModal() {
-  var modal = document.getElementById("tutorialModal");
-  modal.style.display = "none";
-}
-
-// Fechar modal clicando fora da área do vídeo
-window.onclick = function(event) {
-  var modal = document.getElementById("tutorialModal");
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+form.addEventListener('submit', handlePlay);
+playButton.addEventListener('click', handlePlay);
+tutorialButton.addEventListener('click', showTutorial);
+document.querySelector('.fechar-popup').addEventListener('click', closeTutorial);
